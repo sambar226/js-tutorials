5672e6f69d765fbdc899df2cd7135a12b73675ec
sambar226 <sameerb22601@gmail.com>
2025-10-22 10:35:34 +0000

    variable class

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,6 @@
-console.log("SAmeer")
+const accountId= 1221212
+let accountEmail="sameer@gmail.com"
+var accountPasswd="34567"
+accountCity="Tumsar"
+
+console.table([accountId,accountEmail,accountPasswd,accountCity])
---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId= 1221212
+let accountEmail="sameer@gmail.com"
+var accountPasswd="34567"
+accountCity="Tumsar"
+let accountState
+
+// accountId=233 no allowed
+ 
+/*
+Var should be avoided due to its  issues with block scope and functional
+*/
+
+console.table([accountId,accountEmail,accountPasswd,accountCity,accountState])
