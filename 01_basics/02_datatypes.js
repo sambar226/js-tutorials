013dc0d5c4983025fc2308aac552a9d9e00ca4b2
sambar226 <sameerb22601@gmail.com>
2025-10-25 09:52:03 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+let name ="Sameer"
+let age= 24
+let city=null   
+
+console.log(typeof city, typeof age,typeof "Sameer" )
+// city will give a typeof object as null is assigned . But if nothing is assigned then it will return undefined
+// (typeof undefined) is always undefined
