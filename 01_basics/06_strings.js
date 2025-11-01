37d2f39a3afa4cd8db2e6533d38651f709546f51
sambar226 <sameerb22601@gmail.com>
2025-11-01 10:12:34 +0000

    strings part 2

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -31,4 +31,28 @@ console.log(url.includes('sam')); // boolean return to check if specific char is
 const testString="sameer-dev-barve-22"
 console.log(testString.split('-')); // returns array of splitted words , we can also put limit on it 
 
+console.log(testString.at(-10));
+// console.log(testString.charAt(-10));no output as charAt() can't handle -ve index
+
+console.log(testString.charCodeAt(0)); // will return UTF-16 unicode and value will range from 0 to 65535
+console.log(testString.codePointAt(0)); // will return unicodes which are more than 0XFFFF (65535)
+
+console.log(testString.endsWith('eer',6)); // its not index but a position starting from 1 not like indexes starting from 0
+
+const paragraph="I think Ruth's dog is cuter than your dog and more healthy!"
+const search="dog";
+const indexOfFirst=paragraph.indexOf(search);
+console.log(`The position of first ${search} in para is ${indexOfFirst}`);// this will return index of first char of that word you are finding
+
+console.log(`The position of second ${search} in para is ${paragraph.indexOf(search,indexOfFirst+1)}`); // getting position of 
+
+const para2="I think Ruth's dog is cuter than your dog and more healthy but not more that Sameer's dog , lets not discuss it!"
+
+console.log(para2.lastIndexOf('dog')); // returns index of last occurance of the specified substring
+// we can also add position of it too , it will check till index+! less than or equal to
+console.log(para2.lastIndexOf('dog',10)); 
+
+
+
+
 
