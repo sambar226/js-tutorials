d42f0d1dbd0176557f55bccd97dee60508313b25
sambar226 <sameerb22601@gmail.com>
2025-11-15 18:53:51 +0000

    numbers and math

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,36 @@
+const score= 400
+console.log(score);
+
+const balance=new Number(100.84214234) // this method is used to specifically assign the type and also provides some prototypes to play with like - toString(),toPrecision()
+// console.log(balance);
+
+//console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(4)); // use it carefully
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++  Math  +++++++++++++++++++++++++++
+
+// console.log(Math.abs(-8)); // converts -ve value to +ve
+// console.log(Math.round(4.5)); 
+// //if you want to control rounding off then use ceil() and floor
+// console.log(Math.ceil(4.2)); // rounding off to next value
+// console.log(Math.floor(4.2)); // rounding off to lower value
+
+console.log(Math.random()); // this random value will always be in between 0 and 1
+console.log((Math.random()*10)+1); //+1 because what if value is 0.04588
+ const min=10
+ const max=20
+ console.log(Math.floor((Math.random()*((max-min)+1))+min)) // to get perfect single digit ramdom number
+ 
+
+
+
+
+
+
+
+ 
